ead73fba93d855611dcee08e99b02db929e3ef59
Alicia Sun <alicia@dhcp-18-189-57-76.dyn.MIT.EDU>
2018-02-08 14:11:38 -0500

    add print tree

--- copula.py
+++ copula.py
@@ -1,8 +1,10 @@
-import scipy
 
-from copulalib import Copula
+import sys
+
+import scipy
 import numpy as np
-import pandas as pd
+from scipy.integrate import quad
+from scipy.optimize import fmin
 
 import utils
 
@@ -11,58 +13,31 @@ import utils
 class CopulaException(Exception):
     pass
 
-class CopulaUtil(object):
-    def __init__(self, full_path , utype, ctype):
-        """Instantiates an instance of the copula object 
-
-        :param utype: the distribution for the univariate, can be 'kde','norm'
-        :param ctype: the type of copula models, can be 'Gaussian','cvine','dvine'
-        :param cname: the choice of copulas, can be 'clayton','gumbel'  
-
-        """
-        self.utype = utype
-        self.ctype = ctype
-        # self.cname = cname
-        self.args = None
-        self.corr_measure = 'Pearson'
-        self.model_data = pd.read_csv(full_path, sep=',', index_col=False, 
-            na_values=['NaN', 'nan', 'NULL', 'null'], low_memory=False)
-        self.n_sample = self.model_data.shape[0]
-        self.n_var = self.model_data.shape[1]
-        self.cdfs,self.unis= self._train_cdf()
-        self._fit_copula()
-
-
-    def _train_cdf(self):
-        """Find the cdf for each columns.
-        :output cdfs: list of cdf function for each variables
-        :output unis: np matrix of data after applying cdf function to each column
-        """
-        cdfs = []
-        unis = np.empty([self.n_sample,self.n_var])
-        count = 0
-        for col in self.model_data:
-            dist = utils.Distribution(column=self.model_data[col].values)
-            dist.name=self.utype
-            cdf = dist.cdf
-            cdfs.append(cdf)
-            unis[:,count]=[cdf(x) for x in list(self.model_data[col].values)]
-            count+=1
-        return cdfs,unis
-
-    def _fit_copula(self):
-        """Fits a copula to the data 
-        :output self.param: linear correlation matrix for Gaussian, Kendall's tau for others
-        """
-        if self.ctype == 'Gaussian':
-            self.param = self.model_data.corr(method='pearson').as_matrix()
-        else:
-            self.param = self.model_data.corr(method='kendall').as_matrix()
-
+class Copula(object):
+    def __init__(self,U,V,theta=None,cname=None,dev=False):
+        """Instantiates an instance of the copula object from a pandas dataframe
         
+        :param data: the data matrix X
+        :param utype: the distribution for the univariate, can be 'kde','norm'
+        :param cname: the choice of copulas, can be 'clayton','gumbel','frank','gaussian'  
+
+        """
+        self.U = U
+        self.V = V
+        self.theta = theta
+        self.cname = cname
+        self.tau = scipy.stats.kendalltau(self.U, self.V)[0]
+        if cname:
+            self._get_parameter()
+            self.cdf = self._get_cdf()
+        if dev:
+            self.derivative=self._get_du()
+
+
 
     def density_gaussian(self,u):
-        """Compute density of gaussian copula"""
+        """Compute density of gaussian copula
+        """
         R = cholesky(self.param)
         x = norm.ppf(u)
         z = solve(R,x.T)
@@ -70,77 +45,195 @@ class CopulaUtil(object):
         y = np.exp(-0.5 * np.sum( np.power(z.T,2) - np.power(x,2) , axis=1 ) - log_sqrt_det_rho)
         return y
 
-    @staticmethod
-    def cdf_gumbel(u,v,theta):
-        """Compute CDF of Gumbel copula"""
-        if theta == 1:
-            pass
+    def _get_parameter(self):
+        """ estimate the parameter (theta) of copula given tau
+        """        
+        
+        if self.cname == 'clayton':
+            if self.tau == 1:
+                self.theta = 10000
+            else:
+                self.theta = 2*self.tau/(1-self.tau)
+            
+        elif self.cname == 'frank':
+            self.theta = -fmin(self._frank_help, -5, disp=False)[0]
+            
+        elif self.cname == 'gumbel':
+            if self.tau == 1:
+                self.theta = 10000
+            else:
+                self.theta = 1/(1-self.tau)
+
+
+
+    def _frank_help(self,alpha):
+        """compute first order debye function to estimate theta
+        """
+
+        debye = lambda t: t/(np.exp(t)-1)
+        debye_value = quad(debye, sys.float_info.epsilon, alpha)[0]/alpha
+        diff = (1-self.tau)/4.0  - (debye(-alpha)-1)/alpha
+        return np.power(diff,2)
+
+
+    def _get_cdf(self):
+        """Compute copula cdf
+        """
+        if self.cname == 'clayton':
+            def cdf(U,V,theta):
+                if theta < 0:
+                    raise ValueError("Theta cannot be than 0 for clayton")
+                elif theta == 0:
+                    return np.multiply(U,V)
+                else:
+                    cdf=[np.power(np.power(U[i],-theta)+np.power(V[i],-theta)-1,-1.0/theta) if U[i]>0 else 0 for i in range(len(U))]
+                    return [max(x,0) for x in cdf]
+            return cdf
+
+        elif self.cname =='frank':
+            def cdf(U,V,theta):
+                if theta < 0:
+                    raise ValueError("Theta cannot be less than 0 for Frank")
+                elif theta == 0:
+                    return np.multiply(U,V)
+                else:
+                    num = np.multiply(np.exp(np.multiply(-theta,U))-1,np.exp(np.multiply(-theta,V))-1)
+                    den = np.exp(-theta)-1
+                    return -1.0/theta*np.log(1+num/den)
+            return cdf
+
+        elif self.cname == 'gumbel':
+            def cdf(U,V,theta):
+                if theta < 1:
+                    raise ValueError("Theta cannot be less than 1 for Gumbel")
+                elif theta == 1:
+                    return np.multiply(U,V)
+                else:
+                    cdfs=[]
+                    for i in range(len(U)):
+                        if U[i] == 0:
+                            cdfs.append(0)
+                        else:
+                            h = np.power(-np.log(U[i]),theta)+np.power(-np.log(V[i]),theta)
+                            h = -np.power(h,1.0/theta)
+                            cdfs.append(np.exp(h))
+                    return cdfs
+            return cdf
+
         else:
-            h = np.power(-np.log(u),theta)+np.power(-np.log(v),theta)
-            h = -np.power(h,1.0/theta)
-            return np.exp(h)
+            raise Exception('Unsupported distribution: ' + str(self.cname))
+            
 
 
-    def du_copula(self,u,v,theta,cname):
+    def _get_du(self):
         """Compute partial derivative of each copula function
         :param theta: single parameter of the Archimedean copula
-        :param cnameL name of the copula function
+        :param cname: name of the copula function
         """
-        if cname == 1:
-            return self.du_clayton(u,v,theta)
-        elif cname ==2:
-            return self.du_frank(u,v,theta)
-        elif cname == 3:
-            return self.du_gumbel(u,v,theta)
+        if self.cname == 'clayton':
+            def du(u,v,theta):
+                if theta == 0:
+                    return v
+                else:
+                    A = pow(u,theta)
+                    B = pow(u,-theta)-1
+                    h = 1+np.multiply(A,B)
+                    h = pow(h,(-1-theta)/theta)
+                    return h
+            return du
+
+        elif self.cname =='frank':
+            def du(u,v,theta):
+                if theta == 0:
+                    return v
+                else:
+                    g = lambda theta,z:-1+np.exp(-np.dot(theta,z))
+                    num = np.multiply(g(u,theta),g(v,theta))+g(v,theta)
+                    den = np.multiply(g(u,theta),g(v,theta))+g(1,theta)
+                    return num/den
+            return du
+
+        elif self.cname == 'gumbel':
+            def du(u,v,theta):
+                if theta == 1:
+                    return v
+                else:
+                    p1 = Copula(u,v,theta,'gumbel').cdf(u,v,theta)
+                    p2 = np.power(np.power(-np.log(u),theta)+np.power(-np.log(v),theta),-1+1.0/theta)
+                    p3 = np.power(-np.log(u),theta-1)
+                    return np.divide(np.multiply(np.multiply(p1,p2),p3),u)
+            return du
         else:
-            pass
+            raise Exception('Unsupported distribution: ' + str(self.cname))
+            
 
-    def du_frank(self,u,v,theta):
-        if theta == 0:
-            return v
-        else:
-            g = lambda theta,z:-1+np.exp(-np.dot(theta,z))
-            num = np.multiply(g(u,theta),g(v,theta))+g(v,theta)
-            den = np.multiply(g(u,theta),g(v,theta))+g(1,theta)
-            return num/den
+    
+    @staticmethod
+    def compute_empirical(u,v):
+        """compute empirical distribution 
+        """
+        z_left,z_right=[],[]
+        L,R=[],[]
+        N = len(u)
+        base = np.linspace(0.0,1.0,50)
+        for k in range(len(base)):
+            left = sum(np.logical_and(u <= base[k],v<= base[k]))/N
+            right = sum(np.logical_and(u >= base[k],v>= base[k]))/N
+            if left>0:
+                z_left.append(base[k])
+                L.append(left/base[k]**2)
+            if right>0:
+                z_right.append(base[k])
+                R.append(right/(1-z_right[k])**2)
+        return z_left,L,z_right,R
 
-    def du_clayton(self,u,v,theta):
-        if theta == 0:
-            return v
-        else:
-            A = pow(u,theta)
-            B = pow(u,-theta)-1
-            h = 1+np.multiply(A,B)
-            h = pow(h,(-1-theta)/theta)
-            return h
 
-    def du_gumbel(self,u,v,theta):
-        if theta == 1:
-            return v
-        else:
-            p1 = cdf_gumbel(u,v,theta)
-            p2 = np.power(np.power(-np.log(u),theta)+np.power(-np.log(v),theta),-1+1.0/theta)
-            p3 = np.power(-np.log(u),theta-1)
-            return np.dot(np.dot(p1,p2),p3)/u
-
-    def select_copula(self,u,v,tau=None):
-        """Select best copula function based on likelihood"""
-
-        theta = [0]*3
-        if not tau:
-            tau = scipy.stats.kendalltau(u,v)
-        if tau < 0:
+    @staticmethod
+    def select_copula(U,V):
+        """Select best copula function based on likelihood
+        """
+        clayton_c = Copula(U,V,cname ='clayton')
+        frank_c = Copula(U,V,cname ='frank')
+        gumbel_c = Copula(U,V,cname ='gumbel')
+        theta_c = [clayton_c.theta,frank_c.theta,gumbel_c.theta]
+        if clayton_c.tau <= 0:
             bestC = 2
-            copula = Copula(u,v,family ='frank')
-            paramC = copula.theta
-        else:
-            theta[0] = Copula(u,v,family='clayton').theta
-            theta[1] = Copula(u,v,family='frank').theta
-            theta[2] = Copula(u,v,family='gumbel').theta
-            bestC = 2
-            paramC = theta[1]
+            paramC = frank_c.theta
+            return bestC,paramC
+        z_left,L,z_right,R = Copula.compute_empirical(U,V)
+        left_dependence,right_dependence=[],[]
+        left_dependence.append(clayton_c.cdf(z_left,z_left,clayton_c.theta)/np.power(z_left,2))
+        left_dependence.append(frank_c.cdf(z_left,z_left,frank_c.theta)/np.power(z_left,2))
+        left_dependence.append(gumbel_c.cdf(z_left,z_left,gumbel_c.theta)/np.power(z_left,2))
+        g = lambda c,z: np.divide(1.0-2*np.asarray(z)+c,np.power(1.0-np.asarray(z),2))
+        right_dependence.append(g(clayton_c.cdf(z_right,z_right,clayton_c.theta),z_right))
+        right_dependence.append(g(frank_c.cdf(z_right,z_right,frank_c.theta),z_right))
+        right_dependence.append(g(gumbel_c.cdf(z_right,z_right,gumbel_c.theta),z_right))
+        #compute L2 distance from empirical distribution
+        cost_L =  [np.sum((L-l)**2) for l in left_dependence]
+        cost_R =  [np.sum((R-r)**2) for r in right_dependence]
+        cost_LR =  cost_L+cost_R
+        bestC = np.argmax(cost_LR)
+        paramC = theta_c[bestC]
         return bestC,paramC
 
+       
+
+
+if __name__ == '__main__':
+    #quick test
+    c0 = Copula([0.1,0.2,0.3,0.4],[0.5,0.6,0.5,0.8],cname='clayton')
+    # print(c0.cdf([0,0.1,0.2],[0,0.1,0.8],c0.theta))
+    
+    c1 = Copula([0.1,0.2,0.3,0.4],[0.5,0.6,0.5,0.8],cname='frank')
+    # print(c1.cdf([0,0.1,0.2],[0,0.1,0.2],c1.theta))
+
+    c2 = Copula([0.1,0.2,0.3,0.4],[0.5,0.6,0.5,0.8],cname='gumbel')
+    # print(c2.cdf([0,0.1,0.2],[0,0.1,0.8],c2.theta))
+
+    U=[0.1,0.2,0.3,0.4]
+    V=[0.2,0.15,0.3,0.5]
+    # print(Copula.select_copula(U,V))
 
 
 
--- vine.py
+++ vine.py
@@ -1,44 +1,88 @@
+
+from random import randint
 import numpy as np
+import pandas as pd
 import scipy 
 
+import utils
 import copula
 
 class Vine(object):
 
-    def __init__(self, copula, y_ind):
+    def __init__(self, path, utype, vtype, y_ind=None):
         """Represents a vine model consisting multiple levels of trees
-        param ctype: the type of copula models, can be 'Gaussian','cvine','dvine'
-        param u_matrix: matrix represents the univariate distribution of size m*n, where m is the number of data points, and n is number of variables
+        param model_data: pandas dataframe given the file path
+        param vtype: the type of vine models, can be 'Gaussian','cvine','dvine'
         param n_var: number of variables
+        param copula: a copula object fitted to the data
+        param cdfs: cdf function fitted to the data
+        param u_matrix: matrix represents the univariate distribution of size m*n, where m is the number of data points, and n is number of variables
         param depth: depth(number of trees) of the vine model
         param tau_mat: the kendall tau matrix
         param y_ind: index of the variable that will be used as y variable
+
         param dat_V: univariate cdf of the output variables
         param dat_U: univariate cdf of the input variables
         param vine_model: array [level of tree] -> [tree]
         """
-        # self.copula = copula
-        self.ctype = copula.ctype
-        self.u_matrix = copula.unis
-        self.n_var = copula.n_var
-        print 'number of variables: %d'%(self.n_var)
+        self.model_data = pd.read_csv(path, sep=',', index_col=False, 
+            na_values=['NaN', 'nan', 'NULL', 'null'], low_memory=False)
+        self.utype = utype
+        self.vtype = vtype
+        self.n_sample = self.model_data.shape[0]
+        self.n_var = self.model_data.shape[1]
         self.depth = self.n_var - 1
-        self.tau_mat = copula.param
-        if y_ind is None:
+        if not y_ind:
             self.y_ind = self.n_var - 1
         else:
             self.y_ind = y_ind
+        print 'number of variables: %d'%(self.n_var)
+
+        """transform copulas into ites univariate"""
+        self.cdfs,self.u_matrix= self._train_cdf()
+        self.tau_mat = None
+        self._fit_copula()
         self.dat_V = self.u_matrix[:,self.y_ind]     
         self.dat_U = np.delete(self.u_matrix, self.y_ind, axis=1) 
+        self.c_map = {0:'clayton', 1:'frank', 2:'gumbel'}
+
         self.vine_model=[]
+
+    def _train_cdf(self):
+        """Find the cdf for each columns.
+        :output cdfs: list of cdf function for each variables
+        :output unis: np matrix of data after applying cdf function to each column
+        """
+        cdfs = []
+        unis = np.empty([self.n_sample,self.n_var])
+        count = 0
+        for col in self.model_data:
+            dist = utils.Distribution(column=self.model_data[col].values)
+            dist.name=self.utype
+            cdf = dist.cdf
+            cdfs.append(cdf)
+            unis[:,count]=[cdf(x) for x in list(self.model_data[col].values)]
+            count+=1
+        return cdfs,unis
+
+    def _fit_copula(self):
+        """Fits a copula to the data 
+        :output self.param: linear correlation matrix for Gaussian, Kendall's tau for others
+        """
+        if self.vtype == 'Gaussian':
+            self.tau_mat = self.model_data.corr(method='pearson').as_matrix()
+        else:
+            self.tau_mat = self.model_data.corr(method='kendall').as_matrix()
 
     def train_vine(self):
         """Train a vine model
         output: trees are stored in self.vine_model
         """
         tree_1 = Tree(0,self)
-        print('finish building tree : 0')
         self.vine_model.append(tree_1)
+
+        print('finish building tree : 0')
+        tree_1.print_tree()
         self.u_matrix = tree_1.new_U
 
         for k in range(1,self.depth):
@@ -48,16 +92,17 @@ class Vine(object):
             self.tau_mat = tau
             tree_k = Tree(k,self)
             self.vine_model.append(tree_k)
-            self.u_matrix = tree_k.new_U
+
             print'finish building tree: %d'%(k)
+            tree_k.print_tree()
+
+            self.u_matrix = tree_k.new_U
 
 
 
 
     def _get_constraints(self,tree):
-        """
-
-
+        """Get constraints for next tree
         """
         tree_data = tree.tree_data
         constraints = []
@@ -65,6 +110,7 @@ class Vine(object):
             const_k = []
             for i in range(tree.n_nodes-1):
                 if k!=i:
+                    #add to constriants if shared an edge with k
                     if tree_data[k,1]==tree_data[i,1] or tree_data[k,2]==tree_data[i,2] or tree_data[k,1]==tree_data[i,2] or tree_data[k,2]==tree_data[i,1]:
                         const_k.append(i)
             constraints.append(const_k)
@@ -83,6 +129,14 @@ class Vine(object):
     def predict(self,test_data):
         """compute univariate distribution for the test data"""
         copula = copula.CopulaUtil()
+
+    def sampling(self):
+        """generating samples from vine model"""
+        unis = np.random.uniform(0,1,self.n_var)
+        first_ind = randint(0,self.n_var)
+
+
+
 
 
 
@@ -105,7 +159,6 @@ class Tree():
             self.n_nodes = self.vine.n_var
             self.tree_data = np.empty([self.n_nodes-1,6])
             self._build_first_tree()
-            self.print_tree()
 
         else:
             self.prev_T = self.vine.vine_model[k-1]
@@ -133,8 +186,9 @@ class Tree():
 
 
     def _build_first_tree(self):
-        """build the first tree with n-1 variable"""
-        """find the pair of maximum tau"""
+        """build the first tree with n-1 variable
+        """
+        #find the pair of maximum tau
         tau_mat = self.vine.tau_mat
         np.fill_diagonal(tau_mat,np.NaN)
         tau_y = tau_mat[:,self.vine.y_ind]
@@ -145,7 +199,7 @@ class Tree():
         temp[:,2] = abs(tau_y)
         temp[np.isnan(temp)] = -10
         tau_sorted = temp[temp[:,2].argsort()[::-1]]
-        if self.vine.ctype == 'cvine':
+        if self.vine.vtype == 'cvine':
             """for T1, the anchor node is Y"""
             self.tree_data[:,0] = np.arange(self.n_nodes-1)
             self.tree_data[:,1] = self.vine.y_ind
@@ -153,9 +207,10 @@ class Tree():
             self.tree_data[:,2] = np.delete(tau_sorted[:,0],-1,0)
             self.tree_data[:,3] = np.delete(tau_sorted[:,1],-1,0)
             for k in xrange(self.n_nodes-1):
-                self.tree_data[k,4],self.tree_data[k,5]=copula.select_copula(self.vine.u_matrix[:,self.vine.y_ind],self.vine.u_matrix[:,int(self.tree_data[k,2])],self.tree_data[k,3])
+                cop = copula.Copula(self.vine.u_matrix[:,self.vine.y_ind],self.vine.u_matrix[:,int(self.tree_data[k,2])],self.tree_data[k,3])
+                self.tree_data[k,4],self.tree_data[k,5]=cop.select_copula(cop.U,cop.V)
             
-        if self.vine.ctype == 'dvine':
+        if self.vine.vtype == 'dvine':
             
             left_ind = tau_sorted[0,0]
             right_ind = tau_sorted[1,0]
@@ -185,13 +240,15 @@ class Tree():
                 self.tree_data[k,3]=tau_T1[k]
                 '''Select copula function based on upper and lower tail functions'''
                 # print(Copula.select_copula(self.u_matrix[:,T1[k]],self.u_matrix[:,T1[k+1]]))
-                self.tree_data[k,4],self.tree_data[k,5]=copula.select_copula(self.vine.u_matrix[:,T1[k]],self.vine.u_matrix[:,T1[k+1]])
+                cop = copula.Copula(self.vine.u_matrix[:,T1[k]],self.vine.u_matrix[:,T1[k+1]])
+                self.tree_data[k,4],self.tree_data[k,5]=cop.select_copula(cop.U,cop.V)
 
 
 
     def _build_kth_tree(self):
-        """build tree for level k"""
-        if self.vine.ctype == 'cvine':
+        """build tree for level k
+        """
+        if self.vine.vtype == 'cvine':
             """find anchor variable which has the highest sum of dependence with the rest"""
             temp=np.empty([self.n_nodes,2])
             temp[:,0] = np.arange(self.n_nodes,dtype=int)
@@ -211,7 +268,7 @@ class Tree():
             self.tree_data[:,2] = np.delete(tau_sorted[:,0],-1,0)
             self.tree_data[:,3] = np.delete(tau_sorted[:,1],-1,0)
 
-        if self.vine.ctype == 'dvine':
+        if self.vine.vtype == 'dvine':
             for k in xrange(self.n_nodes-1):
                 self.tree_data[k,0]=k
                 self.tree_data[k,1],self.tree_data[k,2]=int(k),int(k+1)
@@ -222,27 +279,34 @@ class Tree():
                 [ed1,ed2,ing] = self.identify_eds_ing(self.prev_T.tree_data[k,1:3],self.prev_T.tree_data[k+1,1:3])
                 U1 = self.vine.u_matrix[ed1,ing]
                 U2 = self.vine.u_matrix[ed2,ing]
-                self.tree_data[k,4],self.tree_data[k,5] = copula.select_copula(U1,U2,self.tree_data[k,3])
+                cop = copula.Copula(U1,U2,self.tree_data[k,3])
+                self.tree_data[k,4],self.tree_data[k,5] = cop.select_copula(cop.U,cop.V)
                 self.tree_data[k,6],self.tree_data[k,7] = ed1, ed2
                 self.tree_data[k,8] = ing
 
     
     def _data4next_T(self,tree):
+        """
+        prepare conditional tau matrix for next tree
+        """
         eps = np.finfo(np.float32).eps
-        U = np.empty([tree.shape[0]+1,tree.shape[0]+1],dtype=object)
-        # print(tree)
-
+        n = self.vine.n_var
+        U = np.empty([n,n],dtype=object)
+        # U = self.vine.u_matrix
         for k in xrange(tree.shape[0]):
-            copula_name = int(tree[k,4])
+            copula_name = self.vine.c_map[int(tree[k,4])]
             copula_para = tree[k,5]
             if self.level == 1:
                 U1,U2 = self.vine.u_matrix[:,int(tree[k,1])],self.vine.u_matrix[:,int(tree[k,2])]
             else:
                 U1,U2 = self.vine.u_matrix[int(tree[k,6]),int(tree[k,8])],self.vine.u_matrix[int(tree[k,7]),int(tree[k,8])]
             '''compute conditional cdfs C(i|j) = dC(i,j)/duj and dC(i,j)/dui'''
-            U1givenU2 = copula.du_copula(U2,U1,copula_para,copula_name)
-            U2givenU1 = copula.du_copula(U1,U2,copula_para,copula_name)
-            # print(U1givenU2)
+            U1=[x for x in U1 if x is not None]
+            U2=[x for x in U2 if x is not None]
+
+            c1= copula.Copula(U2,U1,theta=copula_para,cname=copula_name,dev=True)
+            U1givenU2 = c1.derivative(U2,U1,copula_para)
+            U2givenU1 = c1.derivative(U1,U2,copula_para)
 
             '''correction of 0 or 1'''
             U1givenU2[U1givenU2==0],U2givenU1[U2givenU1==0]=eps,eps
@@ -253,19 +317,43 @@ class Tree():
         return U
 
     def print_tree(self):
-        if self.vine.ctype == "dvine":
-            tree = list(self.tree_data[:,1].astype(int))
-            tree.append(int(self.tree_data[-1,2]))
-        elif self.vine.ctype == "cvine":
+        """
+        print a instance of tree
+        """
+        if self.vine.vtype == "dvine":
+            first=[]
+            tree_1 = self.vine.vine_model[0].tree_data
+            for k in range(tree_1.shape[0]):
+                first.append(str(int(tree_1[k,1])))
+            first.append(str(int(tree_1[-1,2])))
+            s=''
+            if self.level == 1:
+                s =''.join(first)
+                s = '---'.join(s)
+            elif self.level == 2:
+                for k in range(self.tree_data.shape[0]+1):
+                    s+=first[k]+','+first[k+self.level-1]+'---'
+                s=s[:-3]
+
+            else:
+                for k in range(self.tree_data.shape[0]+1):
+                    s+=first[k]+','+first[k+self.level-1]+'|'
+                    for i in range(self.level-2):
+                        s+=first[k+i+1]
+                    s+='---'
+                s=s[:-3]
+        elif self.vine.vtype == "cvine":
             print"anchor node is :%d"%(int(self.vine.y_ind))
             tree = list(self.tree_data[:,2].astype(int))
-        print(tree)
+            s=''
+        print(s)
+
+
 
 
 
 
 if __name__ == '__main__':
-    copula = copula.CopulaUtil('lucas0_train.csv','kde','dvine')
-    dvine = Vine(copula,11)
+    dvine = Vine('test_1.csv','kde','dvine')
     dvine.train_vine()
     print(dvine.vine_model[-1].tree_data)
