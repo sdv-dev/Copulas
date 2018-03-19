82931990ab118ea511e92e42ccec98718c3a9964
amontane <andrew@dhcp-18-111-91-95.dyn.MIT.EDU>
2018-03-19 13:31:44 -0400

    added pdf and cdf

--- copulas/GaussianCopula.py
+++ copulas/GaussianCopula.py
@@ -1,5 +1,6 @@
 from MVCopula import MVCopula
 import scipy.stats as st
+import scipy.integrate as integrate
 import pandas as pd
 import numpy as np
 from univariate.GaussianUnivariate import GaussianUnivariate
@@ -33,6 +34,7 @@ class GaussianCopula(MVCopula):
 			distrib = self.distribs[col]
 			# get original distrib's cdf of the column
 			cdf = distrib.get_cdf(X)
+			print(cdf)
 			# get inverse cdf using standard normal
 			res.loc[:,col] = st.norm.ppf(cdf)
 		# for row in range(self.data.shape[0]):
@@ -41,17 +43,25 @@ class GaussianCopula(MVCopula):
 		# 		distrib = self.distribs[col]
 		# 		cdf = distrib.get_cdf(x)
 		# 		res.loc[row, col] = distrib.inverse_cdf(cdf) # TODO: this should be self.ppf
+		print(res.as_matrix())
 		means = [np.mean(res.iloc[:,i].as_matrix()) for i in range(res.shape[1])]
 		return (np.cov(res.as_matrix()), means)
 
 	def get_pdf(self, X):
-		return self.pdf(X, self.means, self.cov_matrix)
+		cov = self.cov_matrix * np.identity(3)
+		return self.pdf(X, self.means, cov)
+
+	def get_cdf(self,X):
+		func = lambda a,b,c: self.get_pdf([X[0],X[1],X[2]])
+		ranges = [[-10000,val] for val in X]
+		return integrate.nquad(func, ranges)[0]
 
 	def sample(self, num_rows=1):
 		res = {}
 		# means = [self.distribs[self.data.iloc[:,i].name].mean for i in range(self.data.shape[1])]
 		means = [0.0]*len(self.cov_matrix)
-		samples = np.random.multivariate_normal(self.means, self.cov_matrix, size=(num_rows,))
+		samples = np.random.multivariate_normal(means, self.cov_matrix, size=(num_rows,))
+		print(samples)
 		# run through cdf and inverse cdf
 		for i in range(self.data.shape[1]):
 			label = self.data.iloc[:,i].name
@@ -81,3 +91,5 @@ if __name__ == '__main__':
 	gc.fit(data)
 	print(gc.sample(num_rows = 1))
 	print(gc.cov_matrix)
+	print(gc.get_pdf(np.array([1,5,9])))
+	print(gc.get_cdf([2, 5, 8]))
