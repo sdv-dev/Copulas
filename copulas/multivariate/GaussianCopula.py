fixing covariance matrix for gaussian copula to use columns as variables instead of rows

copulas/multivariate/GaussianCopula.py
@@ -52,7 +52,7 @@ class GaussianCopula(MVCopula):
             res.loc[:, col] = st.norm.ppf(cdf)
         n = res.shape[1]
         means = [np.mean(res.iloc[:, i].as_matrix()) for i in range(n)]
-        return (np.cov(res.as_matrix()), means, res)
+        return (np.cov(res.as_matrix(), rowvar=False), means, res)
 
     def get_pdf(self, X):
         # make cov positive semi-definite