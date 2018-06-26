56b7bc4ddcb7c061355ea3a2199da272f399a0d2
Carles Sala <c.sala.stq@gmail.com>
2018-06-26 13:08:58 -0400

    Bump version: 0.1.0-dev → 0.1.0

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/Copulas',
-    version='0.1.0-dev',
+    version='0.1.0',
     zip_safe=False,
 )
