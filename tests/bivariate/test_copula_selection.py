ef0f957ca82b23fc097414db094641741b50333e
Manuel Alvarez <manuel@pythiac.com>
2018-07-26 14:31:17 +0200

    Fix build

--- tests/bivariate/test_copula_selection.py
+++ tests/bivariate/test_copula_selection.py
@@ -17,19 +17,22 @@ class TestCopulas(TestCase):
         U = [0.1, 0.2, 0.3, 0.4]
         V = [0.5, 0.6, 0.5, 0.8]
 
-        c0 = Copula(U, V, cname='clayton')
-        result = c0.cdf([0, 0.1, 0.2], [0, 0.1, 0.8], c0.theta)
+        c0 = Copula('clayton')
+        c0.fit(U, V)
+        result = c0.get_cdf()([0, 0.1, 0.2], [0, 0.1, 0.8])
         expected_result = [0, 0.07517146687679954, 0.19881186077542212]
         assert result == expected_result
 
-        c1 = Copula(U, V, cname='frank')
-        result = c1.cdf([0, 0.1, 0.2], [0, 0.1, 0.2], c1.theta)
+        c1 = Copula(cname='frank')
+        c1.fit(U, V)
+        result = c1.get_cdf()([0, 0.1, 0.2], [0, 0.1, 0.2])
         expected_result = np.array([-0., 0.04775112788933559, 0.13167544529480094])
 
         assert (result == expected_result).all()
 
-        c2 = Copula(U, V, cname='gumbel')
-        result = c2.cdf([0, 0.1, 0.2], [0, 0.1, 0.8], c2.theta)
+        c2 = Copula(cname='gumbel')
+        c2.fit(U, V)
+        result = c2.get_cdf()([0, 0.1, 0.2], [0, 0.1, 0.8])
         expected_result = np.array([0.0, 0.042835279521916785, 0.19817042125347709])
         for i in range(3):
             assert result[i] == expected_result[i]
