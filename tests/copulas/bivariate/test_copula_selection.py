907799f22fed59d759b87bfffe1a209494fa8573
Manuel Alvarez <manuel@pythiac.com>
2018-08-14 01:02:40 +0200

    Include commented tests

--- tests/copulas/bivariate/test_copula_selection.py
+++ tests/copulas/bivariate/test_copula_selection.py
@@ -103,21 +103,21 @@ class TestCopulas(TestCase):
         result = self.c2.sample(10)
         assert result.shape[0] == 10
 
-    # def test_copula_selction_negative_tau(self):
-    #     """If tau is negative, should choose frank copula."""
-    #     U = [0.1, 0.2, 0.3, 0.4]
-    #     V = [0.6, 0.5, 0.4, 0.3]
-    #
-    #     name, param = BVCopula.select_copula(U, V)
-    #     expected = 1
-    #
-    #     assert name == expected
-    #
-    # def test_copula_selction(self):
-    #     U = [0.1, 0.2, 0.3, 0.4]
-    #     V = [1, 2, 3, 4]
-    #
-    #     name, param = BVCopula.select_copula(U, V)
-    #     expected = 1
-    #
-    #     assert name == expected
+    def test_copula_selction_negative_tau(self):
+        """If tau is negative, should choose frank copula."""
+        U = [0.1, 0.2, 0.3, 0.4]
+        V = [0.6, 0.5, 0.4, 0.3]
+    
+        name, param = Bivariate.select_copula(U, V)
+        expected = CopulaTypes.FRANK
+    
+        assert name == expected
+    
+    def test_copula_selction(self):
+        U = [0.1, 0.2, 0.3, 0.4]
+        V = [1, 2, 3, 4]
+    
+        name, param = Bivariate.select_copula(U, V)
+        expected = CopulaTypes.CLAYTON.value
+    
+        assert name == expected
