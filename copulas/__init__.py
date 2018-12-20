bd6a6716d609d5ff7038d53a25f513d4fd1b3d96
Manuel Alvarez <manuel@pythiac.com>
2018-12-20 13:18:54 +0100

    Add random_state decorator

--- copulas/__init__.py
+++ copulas/__init__.py
@@ -9,3 +9,20 @@ __version__ = '0.2.1-dev'
 import numpy as np
 
 EPSILON = np.finfo(np.float32).eps
+
+
+def random_state(function):
+    def wrapper(self, *args, **kwargs):
+        if self.random_seed is None:
+            return function(self, *args, **kwargs)
+
+        else:
+            original_state = np.random.get_state()
+            np.random.seed(self.random_seed)
+
+            result = function(self, *args, **kwargs)
+
+            np.random.set_state(original_state)
+            return result
+
+    return wrapper
