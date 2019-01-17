cb6bdf51678ca175a6e4a49244d6f2060f183c85
Carles Sala <carles@pythiac.com>
2019-01-17 13:35:36 +0100

    Bump version: 0.2.1 → 0.2.2-dev

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/Copulas',
-    version='0.2.1',
+    version='0.2.2-dev',
     zip_safe=False,
 )
