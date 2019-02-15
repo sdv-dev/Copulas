4067238f51443ff3609ae52be68909724195c728
Manuel Alvarez <manuel@pythiac.com>
2019-02-15 17:26:30 +0100

    Stricted comparisions on test helper functions

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+COMPARE_VALUES_ERROR = 'Values don\'t match at index {}\n {} != {}'
+
 
 def compare_nested_dicts(first, second, epsilon=10E-6):
     """Compares two dictionaries. Raises an assertion error when a difference is found."""
@@ -11,32 +13,37 @@ def compare_nested_dicts(first, second, epsilon=10E-6):
         if isinstance(first[key], dict):
             compare_nested_dicts(first[key], second[key])
 
-        elif isinstance(first[key], np.ndarray):
-            assert (compare_values_epsilon(first[key], second[key])).all()
+        elif isinstance(first[key], (list, np.ndarray, tuple)):
+            compare_nested_iterables(first[key], second[key])
 
         elif isinstance(first[key], pd.DataFrame):
             assert first[key].equals(second[key])
 
         elif isinstance(first[key], float):
-            assert compare_values_epsilon(first[key], second[key])
-
-        elif isinstance(first[key], list):
-            compare_nested_iterables(first[key], second[key])
+            assert compare_values_epsilon(first[key], second[key], key)
 
         else:
             assert first[key] == second[key], "{} doesn't equal {}".format(first[key], second[key])
 
 
-def compare_values_epsilon(first, second, epsilon=10E-6):
-    return abs(first - second) < epsilon
+def compare_values_epsilon(first, second, index=None, epsilon=10E-6,):
+    return abs(first - second) < epsilon, COMPARE_VALUES_ERROR.format(index, first, second)
 
 
 def compare_nested_iterables(first, second, epsilon=10E-6):
 
-    for _first, _second in zip(first, second):
+    assert len(first) == len(second), "Iterables should have the same length to be compared."
 
-        if isinstance(_first, list):
+    for index, (_first, _second) in enumerate(zip(first, second)):
+
+        if isinstance(_first, (list, np.ndarray, tuple)):
             compare_nested_iterables(_first, _second)
 
-        if isinstance(_first, float):
+        elif isinstance(_first, dict):
+            compare_nested_dicts(_first, _second)
+
+        elif isinstance(_first, float):
             assert compare_values_epsilon(_first, _second)
+
+        else:
+            assert _first == _second, COMPARE_VALUES_ERROR.format(index, _first, _second)
