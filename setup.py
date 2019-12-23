33a384d33fc5d285ccb90a79dd59cde42cc602fc
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-12-23 16:43:39 +0100

    Bump version: 0.2.4 → 0.2.5-dev

--- setup.py
+++ setup.py
@@ -99,6 +99,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/Copulas',
-    version='0.2.4',
+    version='0.2.5-dev',
     zip_safe=False,
 )
