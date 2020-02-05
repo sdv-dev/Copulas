80797644e171bd4e7ab6dcb2990c1109da018c1e
Kevin Zhang <kevz@mit.edu>
2020-02-05 18:29:06 -0500

    Added beta distribution

--- copulas/univariate/__init__.py
+++ copulas/univariate/__init__.py
@@ -1,11 +1,13 @@
 from copulas.metrics import ks_statistic
 from copulas.univariate.base import Univariate
+from copulas.univariate.beta import BetaUnivariate
 from copulas.univariate.gaussian import GaussianUnivariate
 from copulas.univariate.gaussian_kde import GaussianKDE
 from copulas.univariate.kde import KDEUnivariate
 from copulas.univariate.truncated_gaussian import TruncatedGaussian
 
 __all__ = (
+    'BetaUnivariate',
     'GaussianKDE',
     'GaussianUnivariate',
     'KDEUnivariate',
@@ -19,7 +21,7 @@ def select_univariate(X):
     This function returns an Univariate model which minimizes the KS statistic.
     """
     best_ks, best_model = float("inf"), None
-    for model in [GaussianUnivariate(), GaussianKDE(), TruncatedGaussian()]:
+    for model in [BetaUnivariate(), GaussianUnivariate(), GaussianKDE(), TruncatedGaussian()]:
         ks = ks_statistic(model, X)
         if ks < best_ks:
             best_ks = ks
---
+++ copulas/univariate/beta.py
@@ -0,0 +1,109 @@
+from scipy.stats import beta
+
+from copulas import check_valid_values
+from copulas.univariate.base import ScipyWrapper
+
+
+class BetaUnivariate(ScipyWrapper):
+    """Wrapper around scipy.stats.beta.
+
+    Documentation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.beta.html
+    """
+
+    model_class = 'beta'
+    unfittable_model = True
+    probability_density = 'pdf'
+    cumulative_distribution = 'cdf'
+    percent_point = 'ppf'
+    sample = 'rvs'
+
+    fitted = False
+    constant_value = None
+    mean = None
+    std = None
+
+    def __init__(self):
+        self.a = None
+        self.b = None
+        self.loc = None
+        self.scale = None
+
+    def _get_model(self):
+        return beta(self.a, self.b, loc=self.loc, scale=self.scale)
+
+    def _fit_beta(self, X):
+        """Fit the beta parameters to the data.
+        """
+        self.a, self.b, self.loc, self.scale = beta.fit(X)
+        self.model = self._get_model()
+
+    @check_valid_values
+    def fit(self, X):
+        """Fit scipy model to an array of values.
+
+        Args:
+            X(`np.ndarray` or `pd.DataFrame`):  Datapoints to be estimated from. Must be 1-d
+
+        Returns:
+            None
+        """
+
+        self.constant_value = self._get_constant_value(X)
+
+        if self.constant_value is None:
+            self._fit_beta(X)
+            self._replace_methods()
+        else:
+            self._replace_constant_methods()
+
+        self.fitted = True
+
+    @classmethod
+    def from_dict(cls, parameters):
+        """Set attributes with provided values.
+
+        Args:
+            parameters(dict): Dictionary containing instance parameters.
+
+        Returns:
+            Truncnorm: Instance populated with given parameters.
+        """
+        instance = cls()
+        instance.fitted = parameters['fitted']
+
+        if instance.fitted:
+            instance.a = parameters['a']
+            instance.b = parameters['b']
+            instance.loc = parameters['loc']
+            instance.scale = parameters['scale']
+
+            if instance.scale == 0.0:
+                instance.constant_value = instance.loc
+                instance._replace_constant_methods()
+
+            else:
+                instance.model = instance._get_model()
+                instance._replace_methods()
+
+        return instance
+
+    def _fit_params(self):
+        """Return attributes from self.model to serialize.
+
+        Returns:
+            dict: Parameters to recreate self.model in its current fit status.
+        """
+        if self.constant_value is not None:
+            return {
+                'a': 0.0,
+                'b': 0.0,
+                'loc': self.constant_value,
+                'scale': 0.0,
+            }
+
+        return {
+            'a': self.a,
+            'b': self.b,
+            'loc': self.loc,
+            'scale': self.scale,
+        }
