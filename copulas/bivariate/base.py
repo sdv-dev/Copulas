Fixed partial derivative API

copulas/bivariate/base.py
@@ -6,8 +6,9 @@ from enum import Enum
 
 import numpy as np
 from scipy import stats
+from scipy.optimize import brentq
 
-from copulas import NotFittedError, random_state
+from copulas import EPSILON, NotFittedError, random_state
 from copulas.bivariate.utils import split_matrix
 
 
@@ -256,26 +257,31 @@ class Bivariate(object):
         return self.cumulative_distribution(X)
 
     def percent_point(self, y, V):
-        """Compute the inverse of conditional cumulative density :math:`C(u|v)^{-1}`.
+        """Compute the inverse of conditional cumulative distribution :math:`C(u|v)^{-1}`.
 
         Args:
-            y(np.ndarray): value of :math:`C(u|v)`.
-            V(np.ndarray): given value of V.
-
-        Returns:
-            np.ndarray: Percentiles for the given values.
-
+            y: `np.ndarray` value of :math:`C(u|v)`.
+            v: `np.ndarray` given value of v.
         """
-        raise NotImplementedError
+        self.check_fit()
+        result = []
+        for _y, _v in zip(y, V):
+            def f(u):
+                return self.partial_derivative_scalar(u, _v) - _y
+            minimum = brentq(f, EPSILON, 1.0)
+            if isinstance(minimum, np.ndarray):
+                minimum = minimum[0]
+            result.append(minimum)
+        return np.array(result)
 
     def ppf(self, y, V):
         """Shortcut to :meth:`percent_point`."""
         return self.percent_point(y, V)
 
-    def partial_derivative(self, X, y=0):
+    def partial_derivative(self, X):
         r"""Compute partial derivative of cumulative distribution.
 
-        The partial derivative of the copula(CDF) is the value of the conditional probability.
+        The partial derivative of the copula(CDF) is the conditional CDF.
 
          .. math:: F(v|u) = \frac{\partial C(u,v)}{\partial u}
 
@@ -289,12 +295,12 @@ class Bivariate(object):
         """
         raise NotImplementedError
 
-    def partial_derivative_scalar(self, U, V, y=0):
+    def partial_derivative_scalar(self, U, V):
         """Compute partial derivative :math:`C(u|v)` of cumulative density of single values."""
         self.check_fit()
 
         X = np.column_stack((U, V))
-        return self.partial_derivative(X, y)
+        return self.partial_derivative(X)
 
     @random_state
     def sample(self, n_samples):

copulas/bivariate/clayton.py
@@ -8,8 +8,8 @@ class Clayton(Bivariate):
     """Class for clayton copula model."""
 
     copula_type = CopulaTypes.CLAYTON
-    theta_interval = [-1, float('inf')]
-    invalid_thetas = [0]
+    theta_interval = [0, float('inf')]
+    invalid_thetas = []
 
     def generator(self, t):
         r"""Compute the generator function for Clayton copula family.
@@ -61,7 +61,7 @@ class Clayton(Bivariate):
         The cumulative density(cdf), or distribution function for the Clayton family of copulas
         correspond to the formula:
 
-        .. math:: C(u,v) = max[(u^{-θ} + v^{-θ} - 1),0]^{-1/θ}
+        .. math:: C(u,v) = (u^{-θ} + v^{-θ} - 1)^{-1/θ}
 
         Args:
             X (numpy.ndarray)
@@ -87,7 +87,7 @@ class Clayton(Bivariate):
                 for i in range(len(U))
             ]
 
-            return np.array([max(x, 0) for x in cdfs])
+            return np.array(cdfs)
 
     def percent_point(self, y, V):
         """Compute the inverse of conditional cumulative distribution :math:`C(u|v)^{-1}`.
@@ -112,10 +112,10 @@ class Clayton(Bivariate):
 
             return np.power((a + b - 1) / b, -1 / self.theta)
 
-    def partial_derivative(self, X, y=0):
+    def partial_derivative(self, X):
         r"""Compute partial derivative of cumulative distribution.
 
-        The partial derivative of the copula(CDF) is the value of the conditional probability.
+        The partial derivative of the copula(CDF) is the conditional CDF.
 
         .. math:: F(v|u) = \frac{\partial C(u,v)}{\partial u} =
             u^{- \theta - 1}(u^{-\theta} + v^{-\theta} - 1)^{-\frac{\theta+1}{\theta}}
@@ -141,7 +141,7 @@ class Clayton(Bivariate):
 
         B = np.power(V, -self.theta) + np.power(U, -self.theta) - 1
         h = np.power(B, (-1 - self.theta) / self.theta)
-        return np.multiply(A, h) - y
+        return np.multiply(A, h)
 
     def compute_theta(self):
         r"""Compute theta parameter using Kendall's tau.

copulas/bivariate/frank.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 import scipy.integrate as integrate
-from scipy.optimize import brentq, least_squares
+from scipy.optimize import least_squares
 
 from copulas import EPSILON
 from copulas.bivariate.base import Bivariate, CopulaTypes
@@ -111,20 +111,12 @@ class Frank(Bivariate):
             return V
 
         else:
-            result = []
-            for _y, _V in zip(y, V):
-                minimum = brentq(self.partial_derivative_scalar, EPSILON, 1.0, args=(_y, _V))
-                if isinstance(minimum, np.ndarray):
-                    minimum = minimum[0]
+            return super().percent_point(y, V)
 
-                result.append(minimum)
-
-            return np.array(result)
-
-    def partial_derivative(self, X, y=0):
+    def partial_derivative(self, X):
         r"""Compute partial derivative of cumulative distribution.
 
-        The partial derivative of the copula(CDF) is the value of the conditional probability.
+        The partial derivative of the copula(CDF) is the conditional CDF.
 
         .. math:: F(v|u) = \frac{\partial}{\partial u}C(u,v) =
             \frac{g(u)g(v) + g(v)}{g(u)g(v) + g(1)}
@@ -147,7 +139,7 @@ class Frank(Bivariate):
         else:
             num = np.multiply(self._g(U), self._g(V)) + self._g(U)
             den = np.multiply(self._g(U), self._g(V)) + self._g(1)
-            return (num / den) - y
+            return num / den
 
     def compute_theta(self):
         r"""Compute theta parameter using Kendall's tau.

copulas/bivariate/independence.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy
 
 from copulas.bivariate.base import Bivariate, CopulaTypes
 from copulas.bivariate.utils import split_matrix
@@ -40,7 +39,7 @@ class Independence(Bivariate):
 
     def probability_density(self, X):
         """Compute the probability density for the independence copula."""
-        return scipy.stats.multivariate_normal.pdf(X, cov=np.identity(2))
+        return np.all((0.0 <= X) & (X <= 1.0), axis=1).astype(float)
 
     def cumulative_distribution(self, X):
         """Compute the cumulative distribution of the independence bivariate copula is the product.
@@ -65,7 +64,8 @@ class Independence(Bivariate):
             X()
 
         """
-        return X
+        _, V = split_matrix(X)
+        return V
 
     def percent_point(self, y, V):
         """Compute the inverse of conditional cumulative distribution :math:`F(u|v)^-1`.