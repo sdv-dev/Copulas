4286b6ce782437c52e290984f9615a5beebd96a1
Kevin Zhang <kevz@mit.edu>
2020-02-27 20:02:49 -0500

    Added end-to-end vine test

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 import numpy as np
 
 from copulas.datasets import load_diverse_univariates, load_three_dimensional
-from copulas.multivariate import GaussianMultivariate
+from copulas.multivariate import GaussianMultivariate, VineCopula
 from copulas.univariate import GaussianKDE
 
 
@@ -17,12 +17,12 @@ class TestEndToEnd(TestCase):
     def tearDown(self):
         self.test_dir.cleanup()
 
-    def test_basic(self):
+    def test_gaussian(self):
         for dataloader in [load_diverse_univariates, load_three_dimensional]:
-            with self.subTest(dataloader=dataloader, univariate="default"):
-                self._kitchen_sink(dataloader())
+            with self.subTest(dataloader=dataloader):
+                self._gaussian(dataloader())
 
-    def _kitchen_sink(self, dataset):
+    def _gaussian(self, dataset):
         """
         For the given dataset, this runs "everything but the kitchen sink" (i.e.
         every feature of GaussianMultivariate that is officially supported) and
@@ -32,7 +32,7 @@ class TestEndToEnd(TestCase):
             dataset.columns[0]: GaussianKDE()  # Use a KDE for the first column
         })
         model.fit(dataset)
-        for N in [10, 1000, 100]:
+        for N in [10, 100, 50]:
             assert len(model.sample(N)) == N
         sampled_data = model.sample(10)
         pdf = model.probability_density(sampled_data)
@@ -42,10 +42,7 @@ class TestEndToEnd(TestCase):
         config = model.to_dict()
         model2 = GaussianMultivariate.from_dict(config)
 
-        print(model.to_dict())
-        print(model2.to_dict())
-
-        for N in [10, 1000, 100]:
+        for N in [10, 100, 50]:
             assert len(model2.sample(N)) == N
         pdf2 = model2.probability_density(sampled_data)
         cdf2 = model2.cumulative_distribution(sampled_data)
@@ -55,9 +52,34 @@ class TestEndToEnd(TestCase):
         path_to_model = os.path.join(self.test_dir.name, "model.pkl")
         model.save(path_to_model)
         model2 = GaussianMultivariate.load(path_to_model)
-        for N in [10, 1000, 100]:
+        for N in [10, 100, 50]:
             assert len(model2.sample(N)) == N
         pdf2 = model2.probability_density(sampled_data)
         cdf2 = model2.cumulative_distribution(sampled_data)
         assert np.all(np.isclose(pdf, pdf2, atol=0.01))
         assert np.all(np.isclose(cdf, cdf2, atol=0.01))
+
+    def test_vine(self):
+        for dataloader in [load_three_dimensional]:
+            with self.subTest(dataloader=dataloader):
+                self._vine(dataloader())
+
+    def _vine(self, dataset):
+        """
+        For the given dataset, this fits and samples using each type of Vine
+        copula and makes sure it doesn't crash.
+        """
+        model = VineCopula('direct')
+        model.fit(dataset)
+        for N in [10, 100, 50]:
+            assert len(model.sample(N)) == N
+
+        model = VineCopula('regular')
+        model.fit(dataset)
+        for N in [10, 100, 50]:
+            assert len(model.sample(N)) == N
+
+        model = VineCopula('center')
+        model.fit(dataset)
+        for N in [10, 100, 50]:
+            assert len(model.sample(N)) == N
