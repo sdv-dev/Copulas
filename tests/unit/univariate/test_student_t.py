c4624bb8cb9b00aad8cd7154cbc612833d36b0c1
gbonomib <gbonomib@gmail.com>
2020-05-28 22:10:33 +0200

    PR fixes 7
    https://github.com/sdv-dev/Copulas/pull/173/files/9026c817ceae5071917e9aad3ff64a86a764834e#r431826276

--- tests/unit/univariate/test_student_t.py
+++ tests/unit/univariate/test_student_t.py
@@ -8,28 +8,19 @@ from copulas.univariate.student_t import StudentTUnivariate
 
 class TestStudentTUnivariate(TestCase):
 
-    def test__fit_constant(self):
-        distribution = StudentTUnivariate()
-
-        distribution._fit_constant(np.array([1, 1, 1, 1]))
-
-        assert distribution._params == {
-            'df': 100,
-            'loc': 1,
-            'scale': 0
-        }
-
     def test__fit(self):
         distribution = StudentTUnivariate()
 
-        data = t.rvs(size=1000, df=3, loc=1, scale=1)
+        data = t.rvs(size=50000, df=3, loc=1, scale=1)
         distribution._fit(data)
 
-        assert distribution._params == {
-            'df': 2.8331725136645227,
-            'loc': 1.0151500951225847,
-            'scale': 1.000966233180422,
+        expected = {
+            'df': 3,
+            'loc': 1,
+            'scale': 1,
         }
+        for key, value in distribution._params.items():
+            np.testing.assert_allclose(value, expected[key], atol=0.1)
 
     def test__is_constant_true(self):
         distribution = StudentTUnivariate()
