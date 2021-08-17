08bcaead45e3b715c3eba84185b6f2263de6f404
Felipe Hofmann <fealho@gmail.com>
2021-08-16 20:14:39 -0700

    Bump version: 0.5.1 → 0.5.2.dev0

--- setup.py
+++ setup.py
@@ -110,6 +110,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/Copulas',
-    version='0.5.1',
+    version='0.5.2.dev0',
     zip_safe=False,
 )
