342dca1845e5d12f0604899e0afa0a29a47344bf
amontanez24 <amontanez2424@gmail.com>
2021-11-05 15:32:35 -0500

    Bump version: 0.6.0 → 0.6.1.dev0

--- setup.py
+++ setup.py
@@ -111,6 +111,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/Copulas',
-    version='0.6.0',
+    version='0.6.1.dev0',
     zip_safe=False,
 )
