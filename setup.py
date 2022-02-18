04d6448b7d181df06429b824461953115fbc08d5
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-02-17 22:51:52 -0500

    Bump version: 0.6.1.dev0 → 0.6.1.dev1

--- setup.py
+++ setup.py
@@ -131,6 +131,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/Copulas',
-    version='0.6.1.dev0',
+    version='0.6.1.dev1',
     zip_safe=False,
 )
