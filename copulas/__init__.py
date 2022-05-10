3a1eb3010248998b244990b7f999498787c891a8
Felipe Hofmann <fealho@gmail.com>
2022-05-10 09:09:15 -0700

    Bump version: 0.7.0.dev1 → 0.7.0

--- copulas/__init__.py
+++ copulas/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com',
-__version__ = '0.7.0.dev1'
+__version__ = '0.7.0'
 
 import contextlib
 import importlib
--- setup.py
+++ setup.py
@@ -131,6 +131,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/Copulas',
-    version='0.7.0.dev1',
+    version='0.7.0',
     zip_safe=False,
 )
