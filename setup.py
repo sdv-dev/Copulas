e707f5ec9e0ca23a18fee33b75cbd844b313e5c9
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-10-04 18:28:21 -0400

    Add support for python 3.10

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="A python library for building different types of copulas and using them for sampling.",
     extras_require={
@@ -126,7 +127,7 @@ setup(
     keywords='copulas',
     name='copulas',
     packages=find_packages(include=['copulas', 'copulas.*']),
-    python_requires='>=3.6,<3.10',
+    python_requires='>=3.6,<3.11',
     setup_requires=setup_requires,
     test_suite='tests',
     tests_require=tests_require,
