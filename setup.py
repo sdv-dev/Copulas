7da03ec97fe2773cd2ef669a5d6504204d5b0bdd
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-10-04 19:32:13 -0400

    Update min numpy version

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ install_requires = [
     "matplotlib>=3.2.0,<3.4;python_version<'3.7'",
     "matplotlib>=3.4.0,<4;python_version>='3.7'",
     "numpy>=1.18.0,<1.20.0;python_version<'3.7'",
-    "numpy>=1.20.0,<2;python_version>='3.7'",
+    "numpy>=1.20.0,<2;python_version=='3.7'",
+    "numpy>=1.22.0,<2;python_version>'3.7'",
     'pandas>=1.1.3,<2',
     'scipy>=1.5.4,<2',
 ]
