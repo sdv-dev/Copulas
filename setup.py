0e57219c40385806d4dc71e197d4259d0c46c4e1
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-01-05 17:01:50 +0100

    Lower pandas requirement for python 3.10

--- setup.py
+++ setup.py
@@ -14,10 +14,11 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
 install_requires = [
     "matplotlib>=3.4.0,<4;python_version>='3.7' and python_version<'3.10'",
     "matplotlib>=3.6.0,<4;python_version>='3.10'",
-    "numpy>=1.20.0,<2;python_version>='3.7' and python_version<'3.10'",
+    "numpy>=1.20.0,<2;python_version<'3.10'",
     "numpy>=1.23.3,<2;python_version>='3.10'",
     "pandas>=1.1.3,<2;python_version<'3.10'",
-    "pandas>=1.5.0,<2;python_version>='3.10'",
+    "pandas>=1.3.4,<2;python_version>='3.10' and python_version<'3.11'",
+    "pandas>=1.5.0,<2;python_version>='3.11'",
     "scipy>=1.5.4,<2;python_version<'3.10'",
     "scipy>=1.9.2,<2;python_version>='3.10'",
 ]
