e8bfc9b41a401390a234e76f71a972010e375a7e
Felipe Alex Hofmann <fealho@gmail.com>
2023-11-08 04:58:42 -0800

    Move visualization functions to `plotly` and get rid of `matplotlib` dependency (#368)

--- setup.py
+++ setup.py
@@ -12,13 +12,12 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
     history = history_file.read()
 
 install_requires = [
-    "matplotlib>=3.4.0,<4;python_version<'3.10'",
-    "matplotlib>=3.6.0,<4;python_version>='3.10'",
     "numpy>=1.20.0,<2;python_version<'3.10'",
     "numpy>=1.23.3,<2;python_version>='3.10'",
     "pandas>=1.1.3;python_version<'3.10'",
     "pandas>=1.3.4;python_version>='3.10' and python_version<'3.11'",
     "pandas>=1.5.0;python_version>='3.11'",
+    'plotly>=5.10.0,<6',
     "scipy>=1.5.4,<2;python_version<'3.10'",
     "scipy>=1.9.2,<2;python_version>='3.10'",
 ]
