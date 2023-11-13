304e3dde048c45dcf86970cc1917da2f0ed56992
Frances Hartwell <frances.hartwell@alteryx.com>
2023-11-13 12:08:32 -0500

    Bump version: 0.9.3.dev0 → 0.10.0.dev0

--- setup.py
+++ setup.py
@@ -135,6 +135,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/Copulas',
-    version='0.9.3.dev0',
+    version='0.10.0.dev0',
     zip_safe=False,
 )
