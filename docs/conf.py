a9f7dd5195d14b018a2c749bfba4eaced2dfe0db
gsheni <gvsheni@gmail.com>
2025-03-24 14:41:44 -0400

    delete cells

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import copulas
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'm2r',
+    'sphinx_mdinclude',
     'nbsphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
@@ -78,7 +78,7 @@ release = copulas.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -97,7 +97,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Readthedocs additions
 html_context = {
