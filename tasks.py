dfc25f3c5704f1f7ccc13552bc1341e10b9ab2af
Frances Hartwell <frances@datacebo.com>
2025-12-23 11:56:20 -0500

    Update minimum tests to install minimum scikit-learn

--- tasks.py
+++ tasks.py
@@ -101,9 +101,10 @@ def _get_minimum_versions(dependencies, python_version):
 @task
 def install_minimum(c):
     with open('pyproject.toml', 'rb') as pyproject_file:
-        pyproject_data = tomli.load(pyproject_file)
+        pyproject_data = tomli.load(pyproject_file).get('project', {})
 
-    dependencies = pyproject_data.get('project', {}).get('dependencies', [])
+    dependencies = pyproject_data.get('dependencies', [])
+    dependencies += pyproject_data.get('optional-dependencies', {}).get('tutorials', [])
     python_version = '.'.join(map(str, sys.version_info[:2]))
     minimum_versions = _get_minimum_versions(dependencies, python_version)
 
